ad0fda7f0d6dae2ae9fef574e139f06f4569940a
root <root@localhost.localdomain>
2019-12-09 17:32:36 +0900

    theme first commit

---
+++ wp-content/themes/mytheme/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function mytheme_setup() {
+	add_theme_support('wp-block-styles');
+
+	// 縦横比を維持したレスポンス
+	add_theme_support('responsive-embeds');
+}
+add_action('after_setup_theme', 'mytheme_setup');
+
+function mytheme_enqueue() {
+	wp_enqueue_style(
+		'mytheme-style',
+		get_stylesheet_uri(),
+		array(),
+		filemtime(get_theme_file_path('style.css'))
+	);
+}
+add_action('wp_enqueue_scripts', 'mytheme_enqueue');
